5cccc3635ca46e79bbc90f6fbbe8bf4bf046fa51
zhio <36.zhibo@gmail.com>
2022-07-26 22:43:31 +0800

    Initial Commit

---
+++ z-compress/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  z-compress
+//
+//  Created by zhio on 2022/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ z-compress/z_compressApp.swift
@@ -0,0 +1,17 @@
+//
+//  z_compressApp.swift
+//  z-compress
+//
+//  Created by zhio on 2022/7/26.
+//
+
+import SwiftUI
+
+@main
+struct z_compressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
